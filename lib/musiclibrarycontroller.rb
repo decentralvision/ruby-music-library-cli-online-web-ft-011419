dc1d7663f700b001fb44310b7849e5e1651b22d2
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-03 03:58:42 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -28,6 +28,8 @@ class MusicLibraryController
           self.list_songs_by_artist
         when 'list genre'
           self.list_songs_by_genre
+        when 'play song'
+          self.play_song
       end
     end
   end
